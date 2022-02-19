efaaa5412168b9b7aa5f418037f60495fa001a06
Lentezz <yaroslav.golovan@gmail.com>
2022-02-19 12:42:51 +0200

    initial commit

--- app/src/main/java/com/univer/public_transport_guide/controller/MainActivity.java
+++ app/src/main/java/com/univer/public_transport_guide/controller/MainActivity.java
@@ -3,14 +3,17 @@ package com.univer.public_transport_guide.controller;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -22,7 +25,7 @@ import com.univer.public_transport_guide.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSignIn, btnRegister;
+    Button btnSignIn, btnRegister, btnSignGuest;
     FirebaseAuth auth;
     FirebaseDatabase db;
     DatabaseReference users;
@@ -35,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnSignIn = findViewById(R.id.btnSignIn);
         btnRegister = findViewById(R.id.btnRegister);
+        btnSignGuest = findViewById(R.id.btnSignGuest);
+
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance("https://public-transport-guide-5a729-default-rtdb.europe-west1.firebasedatabase.app");
         users = db.getReference("users");
@@ -44,52 +49,51 @@ public class MainActivity extends AppCompatActivity {
         //registerLayout = findViewById(R.id.register_layout);
         btnRegister.setOnClickListener(view -> showRegisterWindow());
         btnSignIn.setOnClickListener(view -> showSignInWindow());
+        btnSignGuest.setOnClickListener(view -> redirectToApp());
+
     }
 
     public void showRegisterWindow(){
 
-       // registerLayout = findViewById(R.id.register_layout);
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Register now");
-        dialog.setMessage("Write all now");
 
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View registerWindow = inflater.inflate(R.layout.register_window, null);
+        Dialog dialog = new Dialog(this);
+        //AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setContentView(R.layout.custom_dialog_register);
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_window);
 
-        dialog.setView(registerWindow);
+        MaterialButton btnAdd = dialog.findViewById(R.id.btn_yes);
+        MaterialButton btnNo = dialog.findViewById(R.id.btn_no);
+        ImageView btnClose = dialog.findViewById(R.id.btn_close);
+
+        View registerWindow = dialog.findViewById(R.id.root_vg);
 
         final MaterialEditText name = registerWindow.findViewById(R.id.nameField);
         final MaterialEditText password = registerWindow.findViewById(R.id.passField);
         final MaterialEditText email = registerWindow.findViewById(R.id.emailField);
 
-        dialog.setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.cancel());
-        dialog.setPositiveButton("Add", (dialogInterface, i) -> {
-            if(TextUtils.isEmpty(email.getText().toString())){
-                Snackbar.make(registerLayout, "ERROR email", Snackbar.LENGTH_LONG).show();
-                return;
-            }
-            if(TextUtils.isEmpty(name.getText().toString())){
-                Snackbar.make(registerLayout, "ERROR name", Snackbar.LENGTH_LONG).show();
-                return;
-            }
-            if(password.getText().toString().length() < 6){
-                Snackbar.make(registerLayout, "ERROR password", Snackbar.LENGTH_LONG).show();
-                return;
-            }
-            auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                    .addOnSuccessListener(authResult -> {
-                        User user = new User();
-                        user.setEmail(email.getText().toString());
-                        user.setName(name.getText().toString());
-                        user.setPassword(password.getText().toString());
+        btnClose.setOnClickListener(view -> dialog.dismiss());
+        btnNo.setOnClickListener(view -> dialog.dismiss());
 
-                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(unused -> {
-                            startActivity(new Intent(MainActivity.this, UserActivity.class));
-                            finish();
+        btnAdd.setOnClickListener(new MaterialButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(authResult -> {
+                            User user = new User();
+                            user.setEmail(email.getText().toString());
+                            user.setName(name.getText().toString());
+                            user.setPassword(password.getText().toString());
+
+                            users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(unused -> {
+                                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                                finish();
+                            });
+
                         });
-
-                    });
+            }
         });
+
         dialog.show();
 
     }
@@ -97,7 +101,9 @@ public class MainActivity extends AppCompatActivity {
     public void showSignInWindow(){
 
        // registerLayout = findViewById(R.id.sign_in_layout);
+
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
         dialog.setTitle("Sign now");
         dialog.setMessage("Write all now");
 
@@ -127,6 +133,10 @@ public class MainActivity extends AppCompatActivity {
                     });
         });
         dialog.show();
+    }
+
+    public void redirectToApp(){
+        startActivity(new Intent(MainActivity.this, UserActivity.class));
     }
 
 }
